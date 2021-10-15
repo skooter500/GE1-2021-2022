2e5a0184d008ef5cfef5696c395739dff8dc7077
Bryan Duggan <skooter500@gmail.com>
2021-10-15 15:46:22 +0100

    Friday class

---
+++ GE1 Examples 2021/Assets/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
--- GE1 Examples 2021/Assets/Shooting.cs
+++ GE1 Examples 2021/Assets/Shooting.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public int fireRate = 5;
+    public float fireRate = 5;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void OnEnable()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    bool shooting = false;
+
+    System.Collections.IEnumerator Shoot()
+    {
+        while(true)
+        {
+            if (Input.GetAxis("Fire1") > 0 )     
+            {
+                GameObject b = GameObject.Instantiate<GameObject>(bulletPrefab);
+                b.transform.position = bulletSpawn.transform.position;
+                b.transform.rotation = bulletSpawn.rotation;
+                yield return new WaitForSeconds(1 / (float) fireRate);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
